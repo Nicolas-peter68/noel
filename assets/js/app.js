6bf059f3e7a8bc02a218f2aafa8285b98c98fffa
Nicolas-peter68 <71389477+Nicolas-peter68@users.noreply.github.com>
2020-12-10 17:01:38 +0100

    ajout bouton mute

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,15 @@
 const bgm = document.getElementById("bgm");
 bgm.play();
+function mute(m){
+
+  if(bgm.muted == true){
+      bgm.muted = false;
+      m.classList.add('fa-volume-up')
+      m.classList.remove("fa-volume-mute");
+  }
+  else{
+      bgm.muted = true;
+      m.classList.remove('fa-volume-up')
+      m.classList.add("fa-volume-mute");
+  }
+}
--- assets/js/dist/app.dev.js
+++ assets/js/dist/app.dev.js
@@ -2,3 +2,15 @@
 
 var bgm = document.getElementById("bgm");
 bgm.play();
+
+function mute(m) {
+  if (bgm.muted == true) {
+    bgm.muted = false;
+    m.classList.add('fa-volume-up');
+    m.classList.remove("fa-volume-mute");
+  } else {
+    bgm.muted = true;
+    m.classList.remove('fa-volume-up');
+    m.classList.add("fa-volume-mute");
+  }
+}
